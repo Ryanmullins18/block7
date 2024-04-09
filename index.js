34ce69e615ddaabc72defb2b0455ad0217b95706
macbook <macbook@macbooks-mbp.myfiosgateway.com>
2024-04-08 21:41:31 -0400

    final

---
+++ index.js
@@ -0,0 +1,17 @@
+/*
+1. Research 
+2. Create a string
+3. assign variables
+4. add comments
+5. create a dialog box
+*/
+
+//variables to add later
+let surprise="You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+let y= 5+5;
+let x= 40*1;
+let z= 40-1;
+//create string with all variables
+let surpriseMessage= surprise + "" +y +"-"+ x+ "-"+ z;
+//alert new string for popup
+
